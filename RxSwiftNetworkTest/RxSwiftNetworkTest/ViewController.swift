5eb3473f82f5ad8c38c40b0c1653c7395660fee2
minjae-L <qlqlzbto@naver.com>
2024-07-02 21:22:43 +0900

    RxSwift로 데이터 비동기처리 및 UI그리기

--- RxSwiftNetworkTest/RxSwiftNetworkTest/ViewController.swift
+++ RxSwiftNetworkTest/RxSwiftNetworkTest/ViewController.swift
@@ -11,104 +11,33 @@ import RxCocoa
 import Kingfisher
 
 class ViewController: UIViewController {
-
-    let urlString = "minjae-L/RxSwiftNetworkTest"
     let disposeBag = DisposeBag()
-    private let events = BehaviorRelay<[Events]>(value: [])
-    
+    private let viewModel = ViewModel()
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.refreshControl = UIRefreshControl()
+        tableView.rx
+            .setDelegate(self)
+            .disposed(by: disposeBag)
         
-        tableView.refreshControl?.backgroundColor = UIColor(white: 0.98, alpha: 1.0)
-        tableView.refreshControl?.tintColor = UIColor.darkGray
-        tableView.refreshControl?.attributedTitle = NSAttributedString(string: "Pull to refresh")
-        tableView.refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        viewModel
+            .events
+            .observe(on: MainScheduler.instance)
+            .asObservable()
+            .bind(to: tableView.rx.items(cellIdentifier: "cell", cellType: TableViewCell.self)) {(index, element, cell) in
+                cell.userNameLabel.text = "\(element.actor.userName)"
+                cell.eventTypeLabel.text = "\(element.type)"
+                cell.timeLabel.text = "\(element.createdTime)"
+                cell.userIconImageView.kf.setImage(with: URL(string: element.actor.userIconUrl))
+            }
+            .disposed(by: disposeBag)
         
-        refresh()
-    }
-    @objc private func refresh() {
-        fetchEvents(repo: urlString)
-    }
         
-    private func fetchEvents(repo: String) {
-        DispatchQueue.global(qos: .default).async { [weak self] in
-            let response = Observable.from([repo])
-                .map { urlString -> URL in
-                    return URL(string: "https://api.github.com/repos/\(urlString)/events")!
-                }
-                .map{ url -> URLRequest in
-                    return URLRequest(url: url)
-                }
-                .flatMap { request -> Observable<(response: HTTPURLResponse, data: Data)> in
-                    return URLSession.shared.rx.response(request: request)
-                }.share(replay: 1,scope: .whileConnected)
-                .filter { response, _ in
-                    return 200..<300 ~= response.statusCode
-                }
-                .map { _, data -> [Events] in
-                    let decoder = JSONDecoder()
-                    let events = try? decoder.decode([Events].self, from: data)
-                    return events ?? []
-                }
-                .filter { objects in
-                    return !objects.isEmpty
-                }
-                .subscribe { [weak self] newEvents in
-                    self?.processEvents(newEvents: newEvents)
-                }
-                .disposed(by: self?.disposeBag ?? DisposeBag())
-        }
-    }
-    private func processEvents(newEvents: [Events]) {
-        var updatedEvents = newEvents
-        if updatedEvents.count > 50 {
-            updatedEvents = [Events](updatedEvents.prefix(upTo: 50))
-        }
-        events.accept(updatedEvents)
-        
-        DispatchQueue.main.async {
-            self.tableView.reloadData()
-            self.tableView.refreshControl?.endRefreshing()
-        }
-        
-    }
-    private func convertTimeDateFormatter(string: String) -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "ko_KR")
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-        
-        let date = dateFormatter.date(from: string)
-        let displayDateFormatter = DateFormatter()
-        displayDateFormatter.dateFormat = "yyyy년MM월dd일"
-        return displayDateFormatter.string(from: date ?? Date())
     }
 }
-
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return events.value.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? TableViewCell else {
-            return UITableViewCell()
-        }
-        let event = events.value[indexPath.row]
-        cell.eventTypeLabel.text = event.type
-        cell.userNameLabel.text = event.actor.userName
-        cell.userIconImageView.kf.setImage(with: URL(string: event.actor.userIconUrl))
-        cell.timeLabel.text = convertTimeDateFormatter(string: event.createdTime)
-        return cell
-    }
+extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 130
     }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(events.value[indexPath.row])
-    }
-    
 }
---
+++ RxSwiftNetworkTest/RxSwiftNetworkTest/ViewModel.swift
@@ -0,0 +1,67 @@
+//
+//  ViewModel.swift
+//  RxSwiftNetworkTest
+//
+//  Created by 이민재 on 7/2/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class ViewModel {
+    var events = BehaviorRelay<[Events]>(value: [])
+    let disposeBag = DisposeBag()
+    
+    var refreshLoading = BehaviorRelay<Bool>(value: false)
+    
+    init() {
+        let urlString = "minjae-L/RxSwiftNetworkTest"
+        fetchData(urlString: urlString)
+    }
+    
+    private func fetchData(urlString: String){
+        Observable.from([urlString])
+            .asObservable()
+            .map { string in
+                return URL(string: "https://api.github.com/repos/\(urlString)/events")!
+            }
+            .map { url in
+                return URLRequest(url: url)
+            }
+            .flatMap { urlRequest -> Observable<(response: HTTPURLResponse, data: Data)> in
+                return URLSession.shared.rx.response(request: urlRequest)
+            }
+            .share(replay: 1, scope: .whileConnected)
+            .map { _, data -> [Events] in
+                let decoder = JSONDecoder()
+                let events = try? decoder.decode([Events].self, from: data)
+                print("events: \(events)")
+                return events ?? []
+            }
+            .subscribe { [weak self] newEvents in
+                self?.processEvents(newEvents: newEvents)
+            }
+            .disposed(by: disposeBag)
+    }
+    func updateData() {
+        self.fetchData(urlString: "minjae-L/RxSwiftNetworkTest")
+    }
+    private func processEvents(newEvents: [Events]) {
+        var updatedEvents = newEvents
+        if updatedEvents.count > 50 {
+            updatedEvents = [Events](updatedEvents.prefix(upTo: 50))
+        }
+        events.accept(updatedEvents)
+    }
+    func convertTimeDateFormatter(string: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        
+        let date = dateFormatter.date(from: string)
+        let displayDateFormatter = DateFormatter()
+        displayDateFormatter.dateFormat = "yyyy년MM월dd일\nHH시mm분"
+        return displayDateFormatter.string(from: date ?? Date())
+    }
+}
